dfe73c71dc4de5a8640d5bfe3ed01d2860d511f3
Mamatkarimov ibrohim <ibrohim09.ny@gamil.com>
2026-03-25 22:49:38 +0500

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Layout from './layout/Layout'
-import Home from './components/Home'
-import Section1 from './components/Section1'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index={true} element={<Home />}></Route>
-          <Route path='/product/:id' element={<Section1/>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      
+    </>
   )
 }
 
